a1b310c1728cb19420e8d606daafed2486da63d1
noahbachmann <trainpaths@gmail.com>
2026-04-24 11:12:17 +0200

    feat: add beats() function and first test

--- cards.ts
+++ cards.ts
@@ -1,3 +1,32 @@
-export function hello(): string {
-  return "world";
+export enum CardValues {
+	Sechs = 6,
+	Sieben,
+	Acht,
+	Neun,
+	Banner,
+	Under,
+	Ober,
+	König,
+	Ass
+}
+
+export enum ColorValues {
+	Rosen = 3,
+	Schellen = 2,
+	Schilten = 2,
+	Eicheln = 1
+}
+
+export class Card {
+	constructor(
+		private color: ColorValues,
+		private value: CardValues
+	) {}
+
+	public beats(other: Card): boolean {
+		if (this.value == other.value) {
+			return this.color > other.color
+		}
+		return this.value > other.value
+	}
 }
--- cards_test.ts
+++ cards_test.ts
@@ -1,6 +1,11 @@
-import { assertEquals } from "@std/assert";
-import { hello } from "./cards.ts";
+import { assertEquals } from '@std/assert'
+import { Card, CardValues, ColorValues } from './cards.ts'
 
-Deno.test("hello world", () => {
-  assertEquals(hello(), "world");
-});
+Deno.test('bei karten mit gleichen Wert werden die Farben verglichen', () => {
+	const cardLow = new Card(ColorValues.Eicheln, CardValues.Neun)
+	const cardHigh = new Card(ColorValues.Rosen, CardValues.Neun)
+
+	const checkBeats = cardHigh.beats(cardLow)
+
+	assertEquals(checkBeats, true)
+})
